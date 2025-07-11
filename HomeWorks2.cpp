2fb84514bf51dff5a7121a08147fd64655473d85
Sergey-81 <s_nizhegorodov@mail.ru>
2025-07-12 00:00:32 +0300

    Add files via upload
    Домашнее задание к занятию «Переменные и их типы»

---
+++ HomeWorks2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+
+    std::string rusWord{ };
+
+    int variable1{ 957 };
+
+    int variable{};
+
+    std::cout << "Введите число: \n" << variable1 << std::endl;
+    std::cin >> variable;
+    std::cout << "Вы ввели: \n" << variable << std::endl;
+
+    std::cout << "Ведите любое число \n" << std::endl;
+    std::cin >> variable;
+    std::cout << "Вы ввели: \n" << variable << std::endl;
+
+    std::cout << "Введите слово: \n" << "Привет!" << std::endl;
+    std::cin >> rusWord;
+    std::cout << "Вы ввели: \n" << rusWord << std::endl;
+
+    std::cout << "Введите любое слово: \n" << std::endl;
+    std::cin >> rusWord;
+    std::cout << "Вы ввели: \n" << rusWord << std::endl;
+}
