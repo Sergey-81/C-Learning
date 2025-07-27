b5eb7ff9e28ead4c31b8358eb9549ce556a090dc
Sergey-81 <s_nizhegorodov@mail.ru>
2025-07-27 14:43:47 +0300

    Add files via upload
    Домашнее задание к занятию «Функции и их параметры. Рекурсия»

---
+++ HomeWorks6_2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int function(int value, int power) {
+    int result = 1;
+    for (int i = 0; i < power; i++) {
+        result *= value;
+    }
+    return result;
+}
+
+int main() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int value = 5, power = 2;
+    int result = function(value, power);
+    std::cout << value << " в степени " << power << " = " << result << std::endl;
+
+    value = 3;
+    power = 3;
+    result = function(value, power);
+    std::cout << value << " в степени " << power << " = " << result << std::endl;
+
+    value = 4;
+    power = 4;
+    result = function(value, power);
+    std::cout << value << " в степени " << power << " = " << result << std::endl;
+
+    return 0;
+}
+
---
+++ HomeWorks6_3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int fibonacci(int n) {
+    if (n == 0)
+        return 0;
+    else if (n == 1)
+        return 1;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int num;
+    std::cout << "Введите число: ";
+    std::cin >> num;
+
+    std::cout << "Числа Фибоначчи: ";
+    for (int i = 0; i < num; ++i) {
+        std::cout << fibonacci(i) << " ";
+    }
+
+    return 0;
+}
