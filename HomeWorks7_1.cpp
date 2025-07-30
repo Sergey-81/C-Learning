656595df1c5ded9776ebb8002bd04c8677b03e27
Sergey-81 <s_nizhegorodov@mail.ru>
2025-07-30 13:30:17 +0300

    Add files via upload
    Домашнее задание к занятию «Модель памяти и хранение данных»

---
+++ HomeWorks7_1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    
+    int a;
+    short s;
+    long l;
+    long long ll;
+    float f;
+    double d;
+    long double ld;
+    bool b;
+
+    
+    std::cout << "short: " << &s << " " << sizeof(s) << std::endl;
+    std::cout << "int: " << &a << " " << sizeof(a) << std::endl;
+    std::cout << "long: " << &l << " " << sizeof(l) << std::endl;
+    std::cout << "long long: " << &ll << " " << sizeof(ll) << std::endl;
+    std::cout << "float: " << &f << " " << sizeof(f) << std::endl;
+    std::cout << "double: " << &d << " " << sizeof(d) << std::endl;
+    std::cout << "long double: " << &ld << " " << sizeof(ld) << std::endl;
+    std::cout << "bool: " << static_cast<void*>(&b) << " " << sizeof(b) << std::endl;
+
+    return 0;
+
+}
+
---
+++ HomeWorks7_2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+
+void swap(int& x, int& y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+int main(int argc, char** argv) {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int a = 5, b = 8;
+
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+
+    swap(a, b); 
+
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+
+    return 0;
+}
+
