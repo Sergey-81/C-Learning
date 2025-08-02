d3f6490e0498980c5a700f6f14be1b8e6ed4d3c4
Sergey-81 <s_nizhegorodov@mail.ru>
2025-08-02 17:52:13 +0300

    Add files via upload
    Домашнее задание к занятию «Область видимости переменных и типы памяти. Пространства имён»

---
+++ HomeWorks8_1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+void counting_function() {
+    
+    static int call_count = 0;
+    call_count++; 
+    std::cout << "Количество вызовов функции counting_function(): " << call_count << std::endl;
+
+}
+    
+    int main(int argc, char** argv)
+    {
+        SetConsoleCP(1251);
+        SetConsoleOutputCP(1251);
+
+        for (int i = 0; i < 15; i++) {
+            counting_function();
+        }
+        return 0;
+    }
---
+++ HomeWorks8_2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+
+int calc(int x, int y) {
+    return x + y;
+}
+
+int calc(int x, int y, char a) {  
+    return x - y;
+}
+
+int calc(int x, int y, bool b) {  
+    return x * y;
+}
+
+int calc(int x, int y, double c) {  
+    return x / y;
+}
+
+int main() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int x = 6, y = 9;
+    std::cout << "x = " << x << ", y = " << y << std::endl;
+
+    std::cout << "Сложение: " << calc(x, y) << std::endl;
+    std::cout << "Вычитание: " << calc(x, y, '+') << std::endl;  
+    std::cout << "Умножение: " << calc(x, y, true) << std::endl;
+    std::cout << "Деление: " << calc(x, y, 1.0) << std::endl;
+
+    return 0;
+}
