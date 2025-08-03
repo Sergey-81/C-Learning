fa265f904cdba558d8ae240106531090c99d16fc
Sergey-81 <s_nizhegorodov@mail.ru>
2025-08-03 11:45:36 +0300

    Add files via upload

---
+++ HomeWorks8_2_2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+namespace Calculator {
+    int calc(int x, int y) {
+        return x + y;
+    }
+
+    int calc(int x, int y, char a) {
+        return x - y;
+    }
+
+    int calc(int x, int y, bool b) {
+        return x * y;
+    }
+
+    int calc(int x, int y, double c) {
+        return x / y;
+    }
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int x = 6, y = 9;
+    std::cout << "x = " << x << ", y = " << y << std::endl;
+
+    std::cout << "Сложение: " << Calculator::calc(x, y) << std::endl;
+    std::cout << "Вычитание: " << Calculator::calc(x, y, '+') << std::endl;
+    std::cout << "Умножение: " << Calculator::calc(x, y, true) << std::endl;
+    std::cout << "Деление: " << Calculator::calc(x, y, 1.0) << std::endl;
+
+    return 0;
+}
