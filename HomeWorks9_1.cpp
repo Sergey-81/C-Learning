0d2c6f0e4f486266a0a5bd6a39dd111129b571b8
Sergey-81 <s_nizhegorodov@mail.ru>
2025-08-07 15:40:30 +0300

    Add files via upload

---
+++ HomeWorks9_1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+void print(const int arr[], size_t size) {
+    for (size_t i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int arr1[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int arr2[] = { 6, 5, 4, 8 };
+    int arr3[] = { 1, 4, 3, 7, 5 };
+
+    print(arr1, sizeof(arr1) / sizeof(arr1[0]));
+    print(arr2, sizeof(arr2) / sizeof(arr2[0]));
+    print(arr3, sizeof(arr3) / sizeof(arr3[0]));
+
+    return 0;
+}
---
+++ HomeWorks9_2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+void swap(int* a, int* b) {
+    int temp = *a;  
+    *a = *b;        
+    *b = temp;      
+}
+
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int a = 5, b = 8;
+
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+
+    swap(&a, &b);
+
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+
+    return 0;
+}
