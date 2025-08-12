9baed6a8d407439c40d3f0489ceae8f3c45e9ddc
Sergey-81 <s_nizhegorodov@mail.ru>
2025-08-12 04:13:18 +0300

    Add files via upload

---
+++ HomeWorks11_1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    string firstName, lastName;
+
+    cout << "Введите имя: ";
+    getline(cin, firstName);
+
+    cout << "Введите фамилию: ";
+    getline(cin, lastName);
+
+    string greeting = "Здравствуйте, " + firstName + " " + lastName + "!";
+    cout << greeting << endl;
+
+    return 0;
+}
---
+++ HomeWorks11_2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    string secretWord = "малина"; 
+    string userGuess;
+
+    cout << "Угадайте слово: ";
+
+    while (getline(cin, userGuess)) {
+        if (userGuess == secretWord) {
+            cout << "Правильно! Вы победили! Загаданное слово — " << secretWord << endl;
+            break; 
+        }
+        else {
+            cout << "Неправильно" << endl;
+            cout << "Угадайте слово: ";
+        }
+    }
+
+    return 0;
+}
